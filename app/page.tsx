fd20e363a93729b5d5910dd6e920cc31237d798d
Rjun <rjun5689@gmail.com>
2024-12-15 16:45:15 +1000

    navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="h-auto min-w-full bg-gray-50 text-black flex flex-col items-center p-6">
         
         <div className="text-center mb-8">
-          <h1 className="text-3xl font-bold mb-2">Arjun's Website</h1>
+          <h1 className="text-3xl font-bold mb-2">Arjun &pos Website</h1>
           <p className="text-gray-600 text-sm">15 Dec, 2024</p>
         </div>
 
