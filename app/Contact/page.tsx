2f22c867dec0464af9984914fc4778af84ed066d
Rjun <rjun5689@gmail.com>
2024-12-27 11:18:58 +1000

    test

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -3,15 +3,15 @@ import Navbar from '../navbar';
 export default function Home() {
   return (
     <div>
-      
-      
+
+
 
       <div className="flex flex-col  min-h-screen bg-gray-50 text-black bg-notebook bg-notebook">
-      <div className='pt-8'>
-      <Navbar/>
-      </div>
-      
-        
+        <div className='pt-8'>
+          <Navbar />
+        </div>
+
+
         {/* Social Links */}
 
         <div className="flex space-x-4 pt-11 items-center justify-center">
